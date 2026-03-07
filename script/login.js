5d3cf6532158b6688591762311960a1eb9b70eaf
DevByteR <mdromman417@gmail.com>
2026-03-07 22:51:40 +0600

    Sign In functionality added and redirecting home

---
+++ script/login.js
@@ -0,0 +1,14 @@
+ document.getElementById("sign-in-btn").addEventListener('click',() =>{
+    const inputUserName = document.getElementById("input-username");
+    const inputPassword = document.getElementById("input-password");
+    const userName = inputUserName.value;
+    const password = inputPassword.value;
+    if(userName == "admin" && password ==" admin123"){
+        window.location.href = "home.html";
+    }
+    else{
+        alert("Sign In Failed!! Enter correct Username & Password")
+        return;
+    }
+  
+ })
